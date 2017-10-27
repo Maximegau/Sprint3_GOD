ad29b67fec53939cae0f4c3d70b90a1415e4e676
gfaline <gwenf@mit.edu>
2017-10-26 21:52:06 -0400

    updating to get new stuff

--- Sprint3_GODPjt/Assets/Scripts/ChoicesClass.cs
+++ Sprint3_GODPjt/Assets/Scripts/ChoicesClass.cs
@@ -5,13 +5,46 @@ using UnityEngine;
 public class ChoicesClass : MonoBehaviour {
 
 	//have the actual text
+	public string decision;
 
 	//children
+	private List<ChoicesClass> children = null;
 
 	//whether remains next time or not
+	public bool persistant;
 
 	//conditions that change faith modifier
+	private List<string> conditionals = null;
 
 	//faith given
+	private int faithGiven;
+
+	public ChoicesClass(string dec, bool persist, int fate){
+		decision = dec;
+		persistant = persist;
+		faithGiven = fate;
+	}
+
+	public void addChild(ChoicesClass child){
+		if (children == null){
+			children = new List<ChoicesClass>();
+			children.Add (child);
+		}
+		else
+			children.Add(child)
+				
+	}
+
+	public List<ChoicesClass> getChildren(){
+		return children;
+	}
+
+	public bool stays(){
+		return persistant;
+	}
+
+	public int getFaith(){
+
+	}
 
 }
